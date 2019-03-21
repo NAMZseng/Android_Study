14b2dea240608fbec19e0248dd41e1a928ef5808
NamZseng <nanrong_z@foxmail.com>
2019-03-21 20:41:28 +0800

    添加activity间的跳转机制

--- codes/Lesson6_ActivityDemo/app/src/main/java/com/iot/lesson6_activitydemo/FirstActivity.java
+++ codes/Lesson6_ActivityDemo/app/src/main/java/com/iot/lesson6_activitydemo/FirstActivity.java
@@ -1,12 +1,18 @@
 package com.iot.lesson6_activitydemo;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 
 public class FirstActivity extends AppCompatActivity {
 
     public static  final String TAG = "FirstActivity";
+    private Button btn2 = null;
+    private Button btn3 = null;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -14,6 +20,27 @@ public class FirstActivity extends AppCompatActivity {
         setContentView(R.layout.activity_first);
         // 输出日志信息
         Log.i(TAG, "onCreate: is invoked");
+
+        btn2 = findViewById(R.id.btn2);
+        btn3 = findViewById(R.id.btn3);
+
+        // 添加事件监听
+        btn2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // 创建意图对象，表明从第一个activity跳动第二个activity
+                Intent intent = new Intent(FirstActivity.this, SecondActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        btn3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(FirstActivity.this,ThirdActivity.class);
+                startActivity(intent);
+            }
+        });
 
     }
 
