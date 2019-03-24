9b03b1ef6182ea72c081c9c7ac02c26b97a353c8
NamZseng <nanrong_z@foxmail.com>
2019-03-24 21:05:04 +0800

    Intent的使用

---
+++ codes/Lesson7_RegisterDemo/app/src/main/java/com/iot/lesson7_registerdemo/ChooseCityActivity.java
@@ -0,0 +1,12 @@
+package com.iot.lesson7_registerdemo;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ChooseCityActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
