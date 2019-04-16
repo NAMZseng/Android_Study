17e852e88e47253d79cdd55426d6a877cf9bf2c4
NamZseng <nanrong_z@foxmail.com>
2019-04-16 22:34:36 +0800

    finished
    完善广播监听机制

--- codes/Lesson9_MusicPlayerDemo/app/src/main/java/com/iot/lesson9_musicplayerdemo/MainActivity.java
+++ codes/Lesson9_MusicPlayerDemo/app/src/main/java/com/iot/lesson9_musicplayerdemo/MainActivity.java
@@ -1,13 +1,124 @@
 package com.iot.lesson9_musicplayerdemo;
 
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.view.ViewManager;
+import android.widget.ImageButton;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+    private ImageButton stopBtn = null;
+    private ImageButton playBtn = null;
+    private TextView titleTv = null;
+    private TextView authorTv = null;
+    private MyListener myListener = null;
+    private ActivityReceiver activityReceiver = null;
+    //控制播放、暂停
+    public static final String CONTROL = "com.iot.lesson9_musicplayerdemo.control";
+    //更新界面显示
+    public static final String UPDATE = "com.iot.lesson9_musicplayerdemo.update";
+
+    // 定义音乐的播放状态，0x11代表没有播放；0x12代表正在播放；0x13代表暂停
+    int status = 0x11;
+    String[] titleStrs = new String[]{"生活不止眼前的苟且", "平凡之路"};
+    String[] authorStrs = new String[]{"许巍", "朴树"};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // 控件的初始化可以封装到一个init方法体中
+        stopBtn = findViewById(R.id.stopBtn);
+        playBtn = findViewById(R.id.playBtn);
+        titleTv = findViewById(R.id.titleTv);
+        authorTv = findViewById(R.id.authorTv);
+
+        myListener = new MyListener();
+        activityReceiver = new ActivityReceiver();
+
+        stopBtn.setOnClickListener(myListener);
+        playBtn.setOnClickListener(myListener);
+
+        // 设置过滤条件，只能监听UPDATE类广播
+        IntentFilter filter = new IntentFilter(UPDATE);
+        registerReceiver(activityReceiver, filter);
+
+        Intent intent = new Intent(this, MusicService.class);
+        // 启动后台Service
+        startService(intent);
+    }
+
+    // 自定义的BroadcastReceiver，负责监听从Service传回来的广播
+    private class ActivityReceiver extends BroadcastReceiver {
+
+        @Override
+        public void onReceive(Context context, Intent intent) {
+
+            int update = intent.getIntExtra("update", -1);
+            int current = intent.getIntExtra("current", -1);
+            if (current >= 0) {
+                titleTv.setText(titleStrs[current]);
+                authorTv.setText(authorStrs[current]);
+            }
+            switch (update) {
+                case 0x11:
+                    // 设置状态图标
+                    playBtn.setImageResource(R.drawable.play);
+                    // 设置当前状态为没有播放
+                    status = 0x11;
+                    break;
+
+                // 控制系统进入播放状态
+                case 0x12:
+                    playBtn.setImageResource(R.drawable.pause);
+                    status = 0x12;
+                    break;
+
+                // 控制系统进入暂停状态
+                case 0x13:
+                    playBtn.setImageResource(R.drawable.play);
+                    // 设置当前状态为暂停
+                    status = 0x13;
+                    break;
+                default:
+                    break;
+            }
+        }
+    }
+
+    private class MyListener implements View.OnClickListener {
+
+        @Override
+        public void onClick(View v) {
+            Intent intent = new Intent(CONTROL);
+            switch (v.getId()) {
+                case R.id.playBtn:
+                    // 1: 播放/暂停
+                    intent.putExtra("control", 1);
+                    break;
+                case R.id.stopBtn:
+                    // 2：停止播放
+                    intent.putExtra("control", 2);
+                    break;
+                default:
+                    break;
+            }
+
+            // 发送广播 ，将被Service组件中的serviceReceiver接收到
+            sendBroadcast(intent);
+
+        }
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        unregisterReceiver(activityReceiver);
     }
 }
--- codes/Lesson9_MusicPlayerDemo/app/src/main/java/com/iot/lesson9_musicplayerdemo/MusicService.java
+++ codes/Lesson9_MusicPlayerDemo/app/src/main/java/com/iot/lesson9_musicplayerdemo/MusicService.java
@@ -1,16 +1,130 @@
 package com.iot.lesson9_musicplayerdemo;
 
 import android.app.Service;
+import android.content.BroadcastReceiver;
+import android.content.Context;
 import android.content.Intent;
+import android.content.IntentFilter;
+import android.content.res.AssetFileDescriptor;
+import android.content.res.AssetManager;
+import android.media.MediaPlayer;
 import android.os.IBinder;
 
+import java.io.IOException;
+
 public class MusicService extends Service {
-    public MusicService() {
-    }
+    // 当前的状态,0x11代表没有播放 ；0x12代表正在播放；0x13代表暂停
+    private int status = 0x11;
+    private MediaPlayer mediaPlayer = null;
+    private AssetManager assetManager = null;
+    private ServiceReceiver serviceReceiver = null;
+
+    private String[] musics = new String[]{"life.mp3", "road.mp3"};
+    // 记录当前正在播放音乐的索引
+    private int current = 0;
 
     @Override
     public IBinder onBind(Intent intent) {
-        // TODO: Return the communication channel to the service.
-        throw new UnsupportedOperationException("Not yet implemented");
+        return null;
+    }
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        mediaPlayer = new MediaPlayer();
+        assetManager = getAssets();
+        serviceReceiver = new ServiceReceiver();
+        // 设置过滤条件，只能接收CONTROL类广播
+        IntentFilter intentFilter = new IntentFilter(MainActivity.CONTROL);
+        // 动态注册
+        registerReceiver(serviceReceiver, intentFilter);
+
+        // 为MediaPlayer播放完成事件绑定监听器
+        mediaPlayer.setOnCompletionListener(new MediaPlayer.OnCompletionListener() {
+            @Override
+            public void onCompletion(MediaPlayer mp) {
+                current++;
+                if (current >= 2) {
+                    // 循环播放
+                    current = 0;
+                }
+                // 发送广播通知MainActivity更改文本框
+                Intent sendIntent = new Intent(MainActivity.UPDATE);
+                sendIntent.putExtra("current", current);
+                // 发送广播 ，将被MainActivity组件中的BroadcastReceiver接收到
+                sendBroadcast(sendIntent);
+                // 准备、并播放下一首音乐
+                prepareAndPlay(musics[current]);
+            }
+        });
+    }
+
+    private void prepareAndPlay(String music) {
+        try {
+            // 打开指定音乐文件
+            AssetFileDescriptor afd = assetManager.openFd(music);
+            mediaPlayer.reset();
+            // 使用MediaPlayer加载指定的声音文件。
+            mediaPlayer.setDataSource(afd.getFileDescriptor()
+                    , afd.getStartOffset()
+                    , afd.getLength());
+            // 准备声音
+            mediaPlayer.prepare();
+            // 播放
+            mediaPlayer.start();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private class ServiceReceiver extends BroadcastReceiver {
+        @Override
+        public void onReceive(Context context, Intent intent) {
+            // 当没有取到返回值时，默认将-1返回
+            int control = intent.getIntExtra("control", -1);
+            switch (control) {
+                // 播放或暂停
+                case 1:
+                    // 原来处于没有播放状态
+                    if (status == 0x11) {
+                        // 准备、并播放音乐
+                        prepareAndPlay(musics[current]);
+                        status = 0x12;
+                    }
+                    // 原来处于播放状态
+                    else if (status == 0x12) {
+                        mediaPlayer.pause();// 暂停
+                        status = 0x13;// 改变为暂停状态
+                    }
+                    // 原来处于暂停状态
+                    else if (status == 0x13) {
+                        mediaPlayer.start();// 播放
+                        status = 0x12;// 改变状态
+                    }
+                    break;
+                // 停止声音
+                case 2:
+                    // 如果原来正在播放或暂停
+                    if (status == 0x12 || status == 0x13) {
+                        mediaPlayer.stop();// 停止播放
+                        status = 0x11;
+                    }
+                    break;
+                default:
+                    break;
+            }
+            // 发送广播通知MainActivity更改图标、文本框
+            Intent sendIntent = new Intent(MainActivity.UPDATE);
+            sendIntent.putExtra("update", status);
+            sendIntent.putExtra("current", current);
+            // 发送广播 ，将被MainActivity组件中的BroadcastReceiver接收到
+            sendBroadcast(sendIntent);
+        }
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        unregisterReceiver(serviceReceiver);
     }
 }
