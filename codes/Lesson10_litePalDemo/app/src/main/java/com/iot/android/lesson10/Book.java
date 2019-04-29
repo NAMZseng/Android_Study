4f5ad8fd953f24749a739e32d034cbbaebc354bd
NamZseng <nanrong_z@foxmail.com>
2019-04-29 11:06:02 +0800

    数据库框架LitePal使用Demo

--- codes/Lesson10_litePalDemo/app/src/main/java/com/iot/android/lesson10/Book.java
+++ codes/Lesson10_litePalDemo/app/src/main/java/com/iot/android/lesson10/Book.java
@@ -1,8 +1,45 @@
 package com.iot.android.lesson10;
 
 /**
+ * 图书信息DAO类
  * @author Nanrong Zeng
  * @version 1.0
  */
 public class Book {
+    private int bookId;
+    private String author;
+    private String  publisher;
+    private float price;
+
+    public int getBookId() {
+        return bookId;
+    }
+
+    public void setBookId(int bookId) {
+        this.bookId = bookId;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public String getPublisher() {
+        return publisher;
+    }
+
+    public void setPublisher(String publisher) {
+        this.publisher = publisher;
+    }
+
+    public float getPrice() {
+        return price;
+    }
+
+    public void setPrice(float price) {
+        this.price = price;
+    }
 }
