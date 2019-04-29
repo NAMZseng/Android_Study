58a10ac6dc8b38575a5bedd532690a65c5c42079
NamZseng <nanrong_z@foxmail.com>
2019-04-29 11:58:56 +0800

    删除LibManger

--- codes/Lesson10_litePalDemo/app/src/main/java/com/iot/android/lesson10/Book.java
+++ codes/Lesson10_litePalDemo/app/src/main/java/com/iot/android/lesson10/Book.java
@@ -1,14 +1,16 @@
 package com.iot.android.lesson10;
 
+import org.litepal.crud.LitePalSupport;
+
 /**
- * 图书信息DAO类
+ * 图书信息DAO类,继承LitePalSupport类，完成CRUD操作
  * @author Nanrong Zeng
  * @version 1.0
  */
-public class Book {
+public class Book extends LitePalSupport {
     private int bookId;
     private String author;
-    private String  publisher;
+    private String press;
     private float price;
 
     public int getBookId() {
@@ -27,12 +29,12 @@ public class Book {
         this.author = author;
     }
 
-    public String getPublisher() {
-        return publisher;
+    public String getpress() {
+        return press;
     }
 
-    public void setPublisher(String publisher) {
-        this.publisher = publisher;
+    public void setpress(String press) {
+        this.press = press;
     }
 
     public float getPrice() {
---
+++ codes/Lesson10_litePalDemo/app/src/main/java/com/iot/android/lesson10/MainActivity.java
@@ -0,0 +1,18 @@
+package com.iot.android.lesson10;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+/**
+ * 主界面程序, 设有增删查改的跳转按钮
+ * @author Nanrong Zeng
+ * @version 1.0
+ */
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
