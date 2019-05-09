273ebd33dbf9a047a8b7e9bb3662d35e1f2882ad
NamZseng <nanrong_z@foxmail.com>
2019-05-09 20:09:55 +0800

    mainActivity

--- codes/Lesson11_AccessContent/app/src/main/java/com/iot/android/lesson11_accesscontent/MainActivity.java
+++ codes/Lesson11_AccessContent/app/src/main/java/com/iot/android/lesson11_accesscontent/MainActivity.java
@@ -38,15 +38,82 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        // TODO 私有方法
+        // TODO 私有方法可调用？
         init();
 
         titleLL.setVisibility(View.INVISIBLE);
-        resolver = getContentResolver();
 
         MyOnClickListener myOnClickListener = new MyOnClickListener();
         addBtn.setOnClickListener(myOnClickListener);
         queryBtn.setOnClickListener(myOnClickListener);
+    }
+
+    public void addPerson() {
+        String nameStr = nameEt.getText().toString();
+        String numStr = numberEt.getText().toString();
+
+        ContentValues values = new ContentValues();
+        Uri rawContactUri = resolver
+                .insert(ContactsContract.RawContacts.CONTENT_URI, values);
+
+        long contactId = ContentUris.parseId(rawContactUri);
+        // TODO logi
+        System.out.println(contactId);
+
+        // 保存姓名
+        values.clear();
+        values.put(Data.RAW_CONTACT_ID, contactId);
+        values.put(Data.MIMETYPE, StructuredName.CONTENT_ITEM_TYPE);
+        values.put(StructuredName.GIVEN_NAME, nameStr);
+        resolver.insert(android.provider.ContactsContract.Data.CONTENT_URI, values);
+
+        // 保存手机号、电话类型
+        values.clear();
+        values.put(Data.RAW_CONTACT_ID, contactId);
+        values.put(Data.MIMETYPE, Phone.CONTENT_ITEM_TYPE);
+        values.put(Phone.NUMBER, numStr);
+        values.put(Phone.TYPE, Phone.TYPE_MOBILE);
+        resolver.insert(android.provider.ContactsContract.Data.CONTENT_URI, values);
+
+        Toast.makeText(MainActivity.this, "添加成功", 1000).show();
+    }
+
+    public ArrayList<Map<String, String>> queryPerson() {
+        ArrayList<Map<String, String>> detail = new ArrayList<Map<String, String>>();
+        // 获取全部联系人信息
+        Cursor cursor = resolver.query(
+                ContactsContract.Contacts.CONTENT_URI,
+                null, null, null, null);
+        while (cursor.moveToNext()) {
+            Map<String, String> person = new HashMap<String, String>();
+
+            // 获取联系人姓名
+            String personId = cursor.getString(
+                    cursor.getColumnIndex(ContactsContract.Contacts._ID));
+            String name = cursor.getString(
+                    cursor.getColumnIndex(ContactsContract.Contacts.DISPLAY_NAME));
+            person.put("id", personId);
+            person.put("name", name);
+
+            // 获取电话号码
+            Cursor nums = resolver.query(
+                    ContactsContract.CommonDataKinds.Phone.CONTENT_URI, null,
+                    ContactsContract.CommonDataKinds.Phone.CONTACT_ID + "="
+                            + personId, null, null);
+            if (nums.moveToNext()) {
+                String num = nums.getString(nums.getColumnIndex(
+                        ContactsContract.CommonDataKinds.Phone.NUMBER));
+                person.put("number", num);
+            }
+
+            nums.close();
+            detail.add(person);
+        }
+        cursor.close();
+        // TODO logi
+        System.out.println(detail);
+
+        return detail;
     }
 
     private class MyOnClickListener implements View.OnClickListener {
@@ -59,10 +126,12 @@ public class MainActivity extends AppCompatActivity {
                     titleLL.setVisibility(View.VISIBLE);
                     ArrayList<Map<String, String>> persons = queryPerson();
 
-                    SimpleAdapter adapter = new SimpleAdapter(MainActivity.this,
-                            persons, R.layout.result,
-                            new String[] { "id", "name", "num" },
-                            new int[] { R.id.personid, R.id.personname, R.id.personnum });
+                    SimpleAdapter adapter = new SimpleAdapter(
+                            MainActivity.this,
+                            persons,
+                            R.layout.result,
+                            new String[] { "id", "name", "number" },
+                            new int[] { R.id.idTv, R.id.nameTv, R.id.numTv });
 
                     resultLv.setAdapter(adapter);
                     break;
@@ -72,51 +141,6 @@ public class MainActivity extends AppCompatActivity {
         }
     }
 
-    public void addPerson() {
-        String nameStr = nameEt.getText().toString();
-        String numStr = numberEt.getText().toString();
-        ContentValues values = new ContentValues();
-        Uri rawContactUri = resolver.insert(ContactsContract.RawContacts.CONTENT_URI, values);
-
-        long contactId = ContentUris.parseId(rawContactUri);
-        System.out.println(contactId);
-        values.clear();
-        values.put(Data.RAW_CONTACT_ID, contactId);
-        values.put(Data.MIMETYPE, StructuredName.CONTENT_ITEM_TYPE);
-        values.put(StructuredName.GIVEN_NAME, nameStr);
-        resolver.insert(android.provider.ContactsContract.Data.CONTENT_URI, values);
-        values.clear();
-        values.put(Data.RAW_CONTACT_ID, contactId);
-        values.put(Data.MIMETYPE, Phone.CONTENT_ITEM_TYPE);
-        values.put(Phone.NUMBER, numStr);
-        values.put(Phone.TYPE, Phone.TYPE_MOBILE);
-        resolver.insert(android.provider.ContactsContract.Data.CONTENT_URI, values);
-        Toast.makeText(MainActivity.this, "Hello", 1000).show();
-    }
-
-    public ArrayList<Map<String, String>> queryPerson() {
-        ArrayList<Map<String, String>> detail = new ArrayList<Map<String, String>>();
-        Cursor cursor = resolver.query(ContactsContract.Contacts.CONTENT_URI, null, null, null, null);
-        while (cursor.moveToNext()) {
-            Map<String, String> person = new HashMap<String, String>();
-            String personId = cursor.getString(cursor.getColumnIndex(ContactsContract.Contacts._ID));
-            String name = cursor.getString(cursor.getColumnIndex(ContactsContract.Contacts.DISPLAY_NAME));
-            person.put("id", personId);
-            person.put("name", name);
-            Cursor nums = resolver.query(ContactsContract.CommonDataKinds.Phone.CONTENT_URI, null,
-                    ContactsContract.CommonDataKinds.Phone.CONTACT_ID + "=" + personId, null, null);
-            if (nums.moveToNext()) {
-                String num = nums.getString(nums.getColumnIndex(ContactsContract.CommonDataKinds.Phone.NUMBER));
-                person.put("num", num);
-            }
-            nums.close();
-            detail.add(person);
-        }
-        cursor.close();
-        System.out.println(detail);
-        return detail;
-    }
-
     private void init() {
         nameEt = findViewById(R.id.nameEt);
         numberEt = findViewById(R.id.numEt);
@@ -124,5 +148,7 @@ public class MainActivity extends AppCompatActivity {
         queryBtn = findViewById(R.id.queryBtn);
         titleLL = findViewById(R.id.titleLL);
         resultLv = findViewById(R.id.resultLv);
+        resolver = getContentResolver();
+
     }
 }
