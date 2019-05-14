3604fe92e948a7f0a2db6fa891586276dd3c9433
NamZseng <nanrong_z@foxmail.com>
2019-05-14 20:06:53 +0800

    RecycleViewDemo

---
+++ codes/Lesson13_RecyclerViewDemo/app/src/main/java/com/iot/android/lesson13_recyclerviewdemo/FruitAdapter.java
@@ -0,0 +1,28 @@
+package com.iot.android.lesson13_recyclerviewdemo;
+
+import android.support.annotation.NonNull;
+import android.support.v7.widget.RecyclerView;
+import android.view.ViewGroup;
+
+/**
+ * @author Nanrong Zeng
+ * @version 1.0
+ */
+public class FruitAdapter extends RecyclerView.Adapter {
+
+    @NonNull
+    @Override
+    public RecyclerView.ViewHolder onCreateViewHolder(@NonNull ViewGroup viewGroup, int i) {
+        return null;
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull RecyclerView.ViewHolder viewHolder, int i) {
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return 0;
+    }
+}
