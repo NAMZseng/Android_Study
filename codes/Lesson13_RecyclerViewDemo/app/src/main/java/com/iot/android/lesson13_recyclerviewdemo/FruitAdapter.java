cf0c3afa607708d2a7e960c1f97baf4f0f85dea1
NamZseng <nanrong_z@foxmail.com>
2019-05-16 20:44:46 +0800

    对瀑布布局中的各项及项中的图片设置事件监听

--- codes/Lesson13_RecyclerViewDemo/app/src/main/java/com/iot/android/lesson13_recyclerviewdemo/FruitAdapter.java
+++ codes/Lesson13_RecyclerViewDemo/app/src/main/java/com/iot/android/lesson13_recyclerviewdemo/FruitAdapter.java
@@ -7,6 +7,7 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -25,12 +26,15 @@ public class FruitAdapter extends RecyclerView.Adapter<FruitAdapter.MyViewHolder
 
     static class MyViewHolder extends RecyclerView.ViewHolder {
 
+        private View fruitView = null;
         private ImageView fruit_image = null;
         private TextView fruit_name = null;
 
         // 显示初始化父类中带参数的构造方法
         public MyViewHolder(@NonNull View itemView) {
             super(itemView);
+
+            fruitView = itemView;
 
             fruit_image = (ImageView) itemView.findViewById(R.id.fruit_Image);
             fruit_name = (TextView) itemView.findViewById(R.id.fruit_name);
@@ -43,7 +47,29 @@ public class FruitAdapter extends RecyclerView.Adapter<FruitAdapter.MyViewHolder
         View view = LayoutInflater.from(viewGroup.getContext())
                 .inflate(R.layout.fruit_item, viewGroup, false);
 
-        MyViewHolder myViewHolder = new MyViewHolder(view);
+        final MyViewHolder myViewHolder = new MyViewHolder(view);
+
+        // 对布局中的某一项进行事件监听（包括水果图片和水果名一整块）
+        myViewHolder.fruitView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // 获取用户点击的位置
+                int position = myViewHolder.getAdapterPosition();
+                Fruit fruit = fruits.get(position);
+                Toast.makeText(v.getContext(), "图片文字块的监听" + fruit.getName(), Toast.LENGTH_LONG).show();
+            }
+        });
+
+        // 对某项中的图片进行单独的事件监听
+        myViewHolder.fruit_image.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                int position = myViewHolder.getAdapterPosition();
+                Fruit fruit = fruits.get(position);
+                Toast.makeText(v.getContext(), "图片的监听" + fruit.getName(), Toast.LENGTH_LONG).show();
+            }
+        });
+
         return myViewHolder;
     }
 
