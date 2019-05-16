2e68004fb86b7dd2c4e1f53c0a9537b2c382876d
NamZseng <nanrong_z@foxmail.com>
2019-05-16 19:50:17 +0800

    将布局由竖直变成水平

--- codes/Lesson13_RecyclerViewDemo/app/src/main/java/com/iot/android/lesson13_recyclerviewdemo/FruitAdapter.java
+++ codes/Lesson13_RecyclerViewDemo/app/src/main/java/com/iot/android/lesson13_recyclerviewdemo/FruitAdapter.java
@@ -2,27 +2,60 @@ package com.iot.android.lesson13_recyclerviewdemo;
 
 import android.support.annotation.NonNull;
 import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * @author Nanrong Zeng
  * @version 1.0
  */
-public class FruitAdapter extends RecyclerView.Adapter {
+public class FruitAdapter extends RecyclerView.Adapter<FruitAdapter.MyViewHolder> {
+
+    private List<Fruit> fruits = null;
+
+    public FruitAdapter(List<Fruit> fruits) {
+        this.fruits = fruits;
+    }
+
+    static class MyViewHolder extends RecyclerView.ViewHolder {
+
+        private ImageView fruit_image = null;
+        private TextView fruit_name = null;
+
+        // 显示初始化父类中带参数的构造方法
+        public MyViewHolder(@NonNull View itemView) {
+            super(itemView);
+
+            fruit_image = (ImageView) itemView.findViewById(R.id.fruit_Image);
+            fruit_name = (TextView) itemView.findViewById(R.id.fruit_name);
+        }
+    }
 
     @NonNull
     @Override
-    public RecyclerView.ViewHolder onCreateViewHolder(@NonNull ViewGroup viewGroup, int i) {
-        return null;
+    public MyViewHolder onCreateViewHolder(@NonNull ViewGroup viewGroup, int i) {
+        View view = LayoutInflater.from(viewGroup.getContext())
+                .inflate(R.layout.fruit_item, viewGroup, false);
+
+        MyViewHolder myViewHolder = new MyViewHolder(view);
+        return myViewHolder;
     }
 
     @Override
-    public void onBindViewHolder(@NonNull RecyclerView.ViewHolder viewHolder, int i) {
-
+    public void onBindViewHolder(@NonNull MyViewHolder myViewHolder, int i) {
+        Fruit fruit = fruits.get(i);
+        myViewHolder.fruit_image.setImageResource(fruit.getImageId());
+        myViewHolder.fruit_name.setText(fruit.getName());
     }
 
     @Override
     public int getItemCount() {
-        return 0;
+        return fruits.size();
     }
 }
--- codes/Lesson13_RecyclerViewDemo/app/src/main/java/com/iot/android/lesson13_recyclerviewdemo/MainActivity.java
+++ codes/Lesson13_RecyclerViewDemo/app/src/main/java/com/iot/android/lesson13_recyclerviewdemo/MainActivity.java
@@ -2,12 +2,68 @@ package com.iot.android.lesson13_recyclerviewdemo;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
+    private RecyclerView recycler_view = null;
+    private List<Fruit> fruitList = null;
+    private FruitAdapter fruitAdapter = null;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        recycler_view = (RecyclerView) findViewById(R.id.recycler_view);
+        fruitList = new ArrayList<Fruit>();
+        initFruits();
+        fruitAdapter = new FruitAdapter(fruitList);
+
+        LinearLayoutManager linearLayoutManager = new LinearLayoutManager(this);
+        linearLayoutManager.setOrientation(LinearLayoutManager.HORIZONTAL);
+        recycler_view.setLayoutManager(linearLayoutManager);
+        recycler_view.setAdapter(fruitAdapter);
+    }
+
+    private void initFruits() {
+        // 重复添加两次
+        for (int i = 0; i <2 ; i++) {
+            Fruit apple = new Fruit("Apple", R.drawable.apple_pic);
+            fruitList.add(apple);
+
+            Fruit banana = new Fruit("Banana", R.drawable.banana_pic);
+            fruitList.add(banana);
+
+            Fruit cherry = new Fruit("cherry", R.drawable.cherry_pic);
+            fruitList.add(cherry);
+
+            Fruit grape = new Fruit("Grape", R.drawable.grape_pic);
+            fruitList.add(grape);
+
+            Fruit mango = new Fruit("Mango", R.drawable.mango_pic);
+            fruitList.add(mango);
+
+            Fruit orange = new Fruit("Orange", R.drawable.orange_pic);
+            fruitList.add(orange);
+
+            Fruit pear = new Fruit("Pear", R.drawable.pear_pic);
+            fruitList.add(pear);
+
+            Fruit pineapple = new Fruit("Pineapple", R.drawable.pineapple_pic);
+            fruitList.add(pineapple);
+
+            Fruit strawberry = new Fruit("Strawberry", R.drawable.strawberry_pic);
+            fruitList.add(strawberry);
+
+            Fruit watermelon = new Fruit("Watermelon", R.drawable.watermelon_pic);
+            fruitList.add(watermelon);
+
+
+        }
+
     }
 }
