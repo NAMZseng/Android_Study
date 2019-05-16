a86143bc799d7874e4109afd5ae53e449757a34c
NamZseng <nanrong_z@foxmail.com>
2019-05-16 20:26:05 +0800

    使用瀑布布局

--- codes/Lesson13_RecyclerViewDemo/app/src/main/java/com/iot/android/lesson13_recyclerviewdemo/MainActivity.java
+++ codes/Lesson13_RecyclerViewDemo/app/src/main/java/com/iot/android/lesson13_recyclerviewdemo/MainActivity.java
@@ -4,9 +4,11 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.StaggeredGridLayoutManager;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
     private RecyclerView recycler_view = null;
@@ -23,47 +25,68 @@ public class MainActivity extends AppCompatActivity {
         initFruits();
         fruitAdapter = new FruitAdapter(fruitList);
 
-        LinearLayoutManager linearLayoutManager = new LinearLayoutManager(this);
-        linearLayoutManager.setOrientation(LinearLayoutManager.HORIZONTAL);
-        recycler_view.setLayoutManager(linearLayoutManager);
+//        LinearLayoutManager linearLayoutManager = new LinearLayoutManager(this);
+//        linearLayoutManager.setOrientation(LinearLayoutManager.HORIZONTAL);
+//        recycler_view.setLayoutManager(linearLayoutManager);
+
+        // 使用瀑布布局管理器
+        // 4列，每列中的元素为垂直排列
+        StaggeredGridLayoutManager staggeredGridLayoutManager =
+                new StaggeredGridLayoutManager(4, StaggeredGridLayoutManager.VERTICAL);
+        recycler_view.setLayoutManager(staggeredGridLayoutManager);
         recycler_view.setAdapter(fruitAdapter);
     }
 
     private void initFruits() {
         // 重复添加两次
-        for (int i = 0; i <2 ; i++) {
-            Fruit apple = new Fruit("Apple", R.drawable.apple_pic);
+        for (int i = 0; i < 2; i++) {
+            Fruit apple = new Fruit(getRandomLengthName("Apple"), R.drawable.apple_pic);
             fruitList.add(apple);
 
-            Fruit banana = new Fruit("Banana", R.drawable.banana_pic);
+            Fruit banana = new Fruit(getRandomLengthName("Banana"), R.drawable.banana_pic);
             fruitList.add(banana);
 
-            Fruit cherry = new Fruit("cherry", R.drawable.cherry_pic);
+            Fruit cherry = new Fruit(getRandomLengthName("cherry"), R.drawable.cherry_pic);
             fruitList.add(cherry);
 
-            Fruit grape = new Fruit("Grape", R.drawable.grape_pic);
+            Fruit grape = new Fruit(getRandomLengthName("Grape"), R.drawable.grape_pic);
             fruitList.add(grape);
 
-            Fruit mango = new Fruit("Mango", R.drawable.mango_pic);
+            Fruit mango = new Fruit(getRandomLengthName("Mango"), R.drawable.mango_pic);
             fruitList.add(mango);
 
-            Fruit orange = new Fruit("Orange", R.drawable.orange_pic);
+            Fruit orange = new Fruit(getRandomLengthName("Orange"), R.drawable.orange_pic);
             fruitList.add(orange);
 
-            Fruit pear = new Fruit("Pear", R.drawable.pear_pic);
+            Fruit pear = new Fruit(getRandomLengthName("Pear"), R.drawable.pear_pic);
             fruitList.add(pear);
 
-            Fruit pineapple = new Fruit("Pineapple", R.drawable.pineapple_pic);
+            Fruit pineapple = new Fruit(getRandomLengthName("Pineapple"), R.drawable.pineapple_pic);
             fruitList.add(pineapple);
 
-            Fruit strawberry = new Fruit("Strawberry", R.drawable.strawberry_pic);
+            Fruit strawberry = new Fruit(getRandomLengthName("Strawberry"), R.drawable.strawberry_pic);
             fruitList.add(strawberry);
 
-            Fruit watermelon = new Fruit("Watermelon", R.drawable.watermelon_pic);
+            Fruit watermelon = new Fruit(getRandomLengthName("Watermelon"), R.drawable.watermelon_pic);
             fruitList.add(watermelon);
-
-
         }
-
     }
+
+    /**
+     * 将name随机拼接多次
+     *
+     * @param name 水果名字
+     * @return 拼接后的新的水果名
+     */
+    private String getRandomLengthName(String name) {
+        Random random = new Random();
+        // 避免产生长度为0, 进行+1处理
+        int len = random.nextInt(8) + 1;
+        StringBuilder stringBuilder = new StringBuilder();
+        for (int i = 0; i < len; i++) {
+            stringBuilder.append(name);
+        }
+        return stringBuilder.toString();
+    }
+
 }
